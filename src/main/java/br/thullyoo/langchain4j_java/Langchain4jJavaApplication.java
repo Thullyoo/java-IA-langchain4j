989fcbe49772be390d6e3c1d1178d5ad25a838aa
thullyo <thullyofilho@gmail.com>
2025-10-28 12:24:19 -0300

    first commit

---
+++ src/main/java/br/thullyoo/langchain4j_java/Langchain4jJavaApplication.java
@@ -0,0 +1,13 @@
+package br.thullyoo.langchain4j_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchain4jJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Langchain4jJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/thullyoo/langchain4j_java/Langchain4jJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.thullyoo.langchain4j_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Langchain4jJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
